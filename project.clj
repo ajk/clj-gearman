60550442d65b743d9212b98edbafb9c84027a1f4
Antti Koskinen <4ntti.koskinen@gmail.com>
2016-10-22 13:42:44 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-gearman "0.1.0"
-  :description "Gearman client and worker in Clojure"
+(defproject clj-gearman "0.2.0"
+  :description "Gearman clients and workers in Clojure"
   :url "https://github.com/ajk/clj-gearman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
